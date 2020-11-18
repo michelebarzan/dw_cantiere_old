2a98a44b3d82a3df4432fd237085f51cd387f17b
Michele Barzan <michele@servizioglobale.it>
2020-11-18 11:31:41 +0100

    nuovi ponti

--- getCustomSelectItems.php
+++ getCustomSelectItems.php
@@ -10,7 +10,7 @@
     if($data_source=="ditte")
         $query="SELECT cantiere_ditte.nome AS label,cantiere_ditte.id_ditta AS [value] FROM cantiere_ditte WHERE eliminata='false' ORDER BY nome";
     if($data_source=="ponti")
-        $query="SELECT ponte as label, ponte as [value] FROM (SELECT 'gen' AS ponte UNION ALL SELECT 'pref' AS ponte UNION ALL SELECT ponte FROM cantiere_ponti WHERE commessa=".$_SESSION['id_commessa'].") AS t ORDER BY ponte";
+        $query="SELECT ponte as label, ponte as [value] FROM (SELECT 'gen' AS ponte UNION ALL SELECT 'pref' AS ponte UNION ALL SELECT 'mag' AS ponte UNION ALL SELECT 'bag' AS ponte UNION ALL SELECT ponte FROM cantiere_ponti WHERE commessa=".$_SESSION['id_commessa'].") AS t ORDER BY ponte";
     if($data_source=="operatori")
         $query="SELECT id_operatore AS [value], CONCAT(cognome,CONCAT(' ',nome)) AS label FROM cantiere_operatori_ditte WHERE ditta=".$_REQUEST['ditta']." AND eliminato='false' ORDER BY cognome";
     
