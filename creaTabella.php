eac3de8b9ea31f483d97f6ea69021ce71c347f99
Michele Barzan <michele@servizioglobale.it>
2020-11-27 09:00:33 +0100

    cantiere ok

--- creaTabella.php
+++ creaTabella.php
@@ -51,12 +51,29 @@
 				echo "<div class='intestazione-tabella-div'>";echo "<span>Commessa: <b>".$_SESSION['commessa']."</b></span>";echo "</div>";
 				echo "<div class='intestazione-tabella-div' style='margin-left:15px'>";
 					echo "<span>Ordinamento attività:</span>";
-					echo '<select id="selectOrdinamentoPrg" onchange="creaTabella()">
-							<option value="posizione DESC">Posizione descrescente</option>
-							<option value="posizione ASC">Posizione crescente</option>
-							<option value="descrizione DESC">Nome descrescente</option>
-							<option value="descrizione ASC">Nome crescente</option>
-						</select>';
+					echo '<select id="selectOrdinamentoPrg" onchange="setCookie(' . htmlspecialchars(json_encode("selectOrdinamentoPrg")) . ',this.value);creaTabella()">';
+						switch ($orderBy)
+						{
+							case 'descrizione ASC':echo '<option value="descrizione ASC">Nome crescente</option>';break;
+							case 'posizione DESC':echo '<option value="posizione DESC">Posizione descrescente</option>';break;
+							case 'posizione ASC':echo '<option value="posizione ASC">Posizione crescente</option>';break;
+							case 'descrizione DESC':echo '<option value="descrizione DESC">Nome descrescente</option>';break;						
+						}
+						if($orderBy!="descrizione ASC")
+							echo '<option value="descrizione ASC">Nome crescente</option>';
+						if($orderBy!="posizione DESC")
+						echo '<option value="posizione DESC">Posizione descrescente</option>';
+							if($orderBy!="posizione ASC")
+						echo '<option value="posizione ASC">Posizione crescente</option>';
+							if($orderBy!="descrizione DESC")
+						echo '<option value="descrizione DESC">Nome descrescente</option>';
+					echo '</select>';
+					/*echo '<select id="selectOrdinamentoPrg" onchange="setCookie(' . htmlspecialchars(json_encode("selectOrdinamentoPrg")) . ',this.value);creaTabella()">';
+						echo '<option value="descrizione ASC">Nome crescente</option>';
+						echo '<option value="posizione DESC">Posizione descrescente</option>';
+						echo '<option value="posizione ASC">Posizione crescente</option>';
+						echo '<option value="descrizione DESC">Nome descrescente</option>';
+					echo '</select>';*/
 				echo "</div>";
 				echo "<button style='margin-left:auto;opacity:0.6;background:#BFD5E1;height:30px;border:1px solid #0765D0;' class='intestazione-tabella-button' onclick='registra()' id='btnRegistra'><span style='color:#0765D0;font-weight:bold'>Registra</span></button>";
 			echo "</div>";
